f486edaccd881556ba67457864749dd881fe6e16
xx7412421-cloud <xx7412421@gmail.com>
2026-06-20 21:31:24 +0800

    feat: export ERC20 transfer handler

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,4 +1,5 @@
 export * from "./generate-erc20-permit";
+export * from "./generate-erc20-transfer";
 export * from "./generate-erc721-permit";
 export * from "./generate-payout-permit";
 export * from "./encode-decode";
