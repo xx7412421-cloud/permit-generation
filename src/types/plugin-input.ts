c71148c905f60b0c001208b502be2d109e603e94
xx7412421-cloud <xx7412421@gmail.com>
2026-06-20 21:31:54 +0800

    feat: add optional transfer setting

--- src/types/plugin-input.ts
+++ src/types/plugin-input.ts
@@ -25,6 +25,7 @@ export const permitGenerationSettingsSchema = T.Object({
   evmNetworkId: T.Number(),
   evmPrivateEncrypted: T.String(),
   permitRequests: T.Array(permitRequestSchema),
+  transfer: T.Optional(T.Boolean()),
 });
 
 export type PermitGenerationSettings = StaticDecode<typeof permitGenerationSettingsSchema>;
